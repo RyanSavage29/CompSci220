a41e6f67b09991b5be817412bf0243e163f26c46
Ryan Strenkowski <rstrenkowski@gmail.com>
2016-05-28 18:37:58 -0400

    Finished Lab 6, will begin working on Project 2

--- Lab6/Mean.java
+++ Lab6/Mean.java
@@ -1,49 +1,98 @@
+/*******************************
+ * Programmer: Ryan Strenkowski
+ * Instructor: Dr. Tao
+ * Course: CSC220-03
+ * Lab6
+ ******************************/ 
+
 import java.util.Scanner;
 
-public class Mean {
-    public static void main(String[] arrstring) {
-        int n;
-        int[] arrn;
-        int[] arrn2;
-        Scanner scanner = new Scanner(System.in);
-        int n2 = 0;
-        int n3 = 0;
-        int n4 = 0;
-        int n5 = 0;
-        int n6 = 0;
-        int n7 = 0;
-        System.out.print("Please enter the number of integers you want to enter: ");
-        n2 = scanner.nextInt();
-        if (n2 % 2 == 0) {
-            arrn = new int[n2 / 2 + 1];
-            arrn2 = new int[n2 / 2];
-        } else {
-            arrn = new int[n2 / 2 + 1];
-            arrn2 = new int[n2 / 2 + 1];
-        }
-        while (n3 <= n2) {
-            System.out.print("Please enter a number: ");
-            if (n3 % 2 == 0 || n3 == 0) {
-                arrn[n4] = scanner.nextInt();
-                ++n4;
-                ++n3;
-                continue;
-            }
-            arrn2[n5] = scanner.nextInt();
-            ++n5;
-            ++n3;
-        }
-        System.out.println("List 1: ");
-        for (n = 0; n < arrn.length; ++n) {
-            System.out.print("" + arrn[n] + " ");
-            n6 += arrn[n];
-        }
-        System.out.println("\nMean of List 1: " + (float)(n6 / arrn.length));
-        System.out.println("List 2: ");
-        for (n = 0; n < arrn2.length; ++n) {
-            System.out.print("" + arrn2[n] + " ");
-            n7 += arrn2[n];
-        }
-        System.out.println("\nMean of List 2: " + (float)(n7 / arrn2.length));
+/*********************************************************************************************************************************
+* Class Mean will create two different arrays of integers based on user inputed values and then print out the mean for each array
+* If block to create the two arrays based on whether n is even or odd
+* While loop used to fill the two arrays with integers inputted by the user
+* For loops used to print out the two arrays and compute the mean of each list
+*********************************************************************************************************************************/
+
+public class Mean
+{
+  public static void main(String [] args)
+  {
+	Scanner scnr;
+	int n, counter, evenCounter, oddCounter, sum1, sum2;
+	int[] L1, L2;
+	
+    scnr = new Scanner(System.in);
+    n = 0;
+    counter = 0;
+    evenCounter = 0;
+    oddCounter = 0;
+    sum1 = 0;
+    sum2 = 0;
+	 
+    System.out.print("Please enter the number of integers you want to enter: ");
+    n = scnr.nextInt();
+	
+	/**************************************************************************************************************
+	* If block that creates array L1 and L2, where the size is determined by n and whether or not n is even or odd
+	**************************************************************************************************************/
+	
+    if (n % 2 == 0)
+    {
+      L1 = new int[n / 2 + 1];
+      L2 = new int[n / 2];
     }
+    else
+    {
+      L1 = new int[n / 2 + 1];
+      L2 = new int[n / 2 + 1];
+    }
+	
+	/***************************************************************************************************************************
+	* While loop that takes user inputed values and seperates them into array L1 or array L2 based on if counter is even or odd
+	***************************************************************************************************************************/
+	
+    while (counter <= n)
+    {
+      System.out.print("Please enter a number: ");
+      if (counter % 2 == 0 || counter == 0)
+      {
+        L1[evenCounter] = scnr.nextInt();
+        evenCounter++;
+        counter++;
+      }
+      else
+      {
+        L2[oddCounter] = scnr.nextInt();
+        oddCounter++;
+        counter++;
+      }
+    }
+	
+	/******************************************************************************
+	* For loop that prints out all of the integers in List 1 and adds them to sum1
+	* Mean of List 1 is then printed out as a float
+	******************************************************************************/
+	
+    System.out.println("List 1: ");
+    for (int i = 0; i < L1.length; i++)
+    {
+      System.out.print("" + L1[i] + " ");
+      sum1 += L1[i];
+    }
+    System.out.println("\nMean of List 1: " + (float) sum1 / L1.length);
+	
+	/******************************************************************************
+	* For loop that prints out all of the integers in List 2 and adds them to sum2
+	* Mean of List 2 is then printed out as a float
+	******************************************************************************/
+	
+    System.out.println("List 2: ");
+    for (int i = 0; i < L2.length; i++)
+    {
+      System.out.print("" + L2[i] + " ");
+      sum2 += L2[i];
+    }
+    System.out.println("\nMean of List 2: " + (float) sum2 / L2.length);
+  }
 }
