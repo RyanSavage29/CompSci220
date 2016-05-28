9fae32bfe4c7d56ececb4158875c999e40256eba
Ryan Strenkowski <rstrenkowski@gmail.com>
2016-05-28 19:16:15 -0400

    Inital Structure for Project2

---
+++ Project2/WordUnscrambler.java
@@ -0,0 +1,19 @@
+/*******************************
+ * Programmer: Ryan Strenkowski
+ * Instructor: Dr. Tao
+ * Course: CSC220-03
+ * Project2
+ ******************************/ 
+ 
+ import java.util.Scanner;
+ import java.util.Random;
+ import java.io.FileInputStream;
+ import java.io.IOException;
+ 
+ public class WordUnscrambler
+ {
+	 public static void main(String args[])
+	 {
+		 
+	 }
+ }
