c0258f231b1249116a75881e6eb6a7de35170631
Ryan Strenkowski <rstrenkowski@gmail.com>
2016-05-28 20:22:38 -0400

    Succesfully created the array and scrambled the word

--- Project2/WordUnscrambler.java
+++ Project2/WordUnscrambler.java
@@ -26,21 +26,43 @@
 		 Random rand = new Random();
 		 boolean run = true;
 		 boolean end = false;
-		 final int TOTAL_WORDS;
-		 int index1, index2;
-		 String unscrambled, scrambled;
+		 final int TOTAL_WORDS = 20;
+		 int indexStart, indexEnd, index1, index2;
+		 String unscrambled, scrambled, firstLetter, secondLetter;
 		 String[] words = new String[TOTAL_WORDS];
 		 
 		 for (int i = 0; i < TOTAL_WORDS; i++)
 		 {
-			 words[i] = textScan.next();
+			 words[i] = textScnr.next();
 		 }
 		 textFile.close();
 		 
 		 while (run)
 		 {
-			 //unscrambled = words[rand.nextInt(TOTAL_WORDS - 1)];
-			 
+			 unscrambled = words[rand.nextInt(TOTAL_WORDS - 1)];
+			 scrambled = unscrambled;
+			 for (int i = 0; i < scrambled.length(); i++)
+			 {
+				 indexStart = rand.nextInt(unscrambled.length() - 1);
+				 indexEnd = rand.nextInt(unscrambled.length() - 1);
+				 while (indexStart == indexEnd)
+				 {
+					 indexEnd = rand.nextInt(unscrambled.length() - 1);
+				 }
+				 firstLetter = scrambled.substring(indexStart, indexStart + 1);
+				 secondLetter = scrambled.substring(indexEnd, indexEnd + 1);
+				 
+				 if (indexStart < indexEnd)
+				 {
+					 scrambled = scrambled.substring(0, indexStart) + secondLetter + scrambled.substring(indexStart + 1, indexEnd) + firstLetter + scrambled.substring(indexEnd + 1);
+				 }
+				 else
+				 {
+					 scrambled = scrambled.substring(0, indexEnd) + firstLetter + scrambled.substring(indexEnd + 1, indexStart) + secondLetter + scrambled.substring(indexStart + 1);
+				 }
+			 }
+			 System.out.println(unscrambled + "\n" + scrambled);
+			 run = false;
 		 }
 	 }
  }
